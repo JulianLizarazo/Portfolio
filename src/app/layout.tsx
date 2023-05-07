530c5f540a52766ff600d5683a325f358b357e6a
JulianLizarazo <julianca20122001@hotmail.com>
2023-05-07 17:04:51 -0500

    first commit, added custom colors and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Archivo } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const archivo = Archivo({ subsets: ['latin'], display: 'swap' })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={archivo.className}>{children}</body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors:{
+      'dark-black': '#151414',
+      'dark-gray': '#3E3636',
+      'dark-red': '#D72323',
+      'dark-white': '#F5EDED',
+      'light-white': '#FBF8F1',
+      'light-gray': '#F7ECDE',
+      'light-brown': '#E9DAC1',
+      'light-blue': '#54BAB9',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
