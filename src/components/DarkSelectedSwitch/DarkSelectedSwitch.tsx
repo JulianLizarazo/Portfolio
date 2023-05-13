ed1856d5568b62175282d67fa70c9426dfe071a9
JulianLizarazo <julianca20122001@hotmail.com>
2023-05-13 17:35:08 -0500

    theme switch with light/dark styles

--- src/components/DarkSelectedSwitch/DarkSelectedSwitch.tsx
+++ src/components/DarkSelectedSwitch/DarkSelectedSwitch.tsx
@@ -4,10 +4,10 @@ const DarkSelectedSwitch = () => {
     return(
 <section className={style.switch}>
       <div className={style.on}>
-      <BsFillMoonFill width={8} height={8} />
+      <BsFillMoonFill width={8} height={8} className="text-dark-white" />
       </div>
       <div className={style.off}>
-      <BsFillSunFill width={8} height={8} />
+      <BsFillSunFill width={8} height={8} className="text-light-blue" />
        
       </div>
       <div className={style.mountain}></div>
--- src/components/LightSelectedSwitch/LightSelectedSwitch.tsx
+++ src/components/LightSelectedSwitch/LightSelectedSwitch.tsx
@@ -4,10 +4,10 @@ const LightSelectedSwitch = () => {
   return (
     <section className={style.switch}>
       <div className={style.on}>
-        <BsFillSunFill width={8} height={8} />
+        <BsFillSunFill width={8} height={8} className="text-light-blue"/>
       </div>
       <div className={style.off}>
-        <BsFillMoonFill width={8} height={8} />
+        <BsFillMoonFill width={8} height={8} className="text-dark-white" />
       </div>
       <div className={style.mountain}></div>
     </section>
