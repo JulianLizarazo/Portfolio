4fb3b78e7a6c6003e62adf70572502b170dbb730
JulianLizarazo <julianca20122001@hotmail.com>
2023-06-14 22:59:36 -0500

    linkedin url updated

--- src/components/ContactButtons.tsx
+++ src/components/ContactButtons.tsx
@@ -17,7 +17,7 @@ const ContactButtons = () => {
       <a
         className="flex items-center gap-1 cursor-pointer border border-solid rounded bg-light-blue border-light-brown text-light-white  dark:bg-dark-white dark:border-dark-gray dark:text-dark-black p-2 lg:bg-light-white lg:border-light-blue lg:text-light-blue lg:dark:bg-dark-black lg:dark:border-dark-white lg:dark:text-dark-white
         lg:hover:bg-light-blue lg:hover:border-light-brown lg:hover:text-light-white lg:dark:hover:bg-dark-white lg:dark:hover:border-dark-gray lg:dark:hover:text-dark-black"
-        href="https://www.linkedin.com/in/julian-pinilla-lizarazo-76a11427b/"
+        href="https://www.linkedin.com/in/julianlizarazo/"
         target="_blank"
       >
         <AiFillLinkedin className="w-10 h-10" />
--- src/containers/Contact.tsx
+++ src/containers/Contact.tsx
@@ -14,7 +14,7 @@ const Contact = () => {
       <a
         className="w-[60%] max-w-[300px] lg:w-[900px] lg:h-[100px] h-20  flex items-center justify-evenly mt-16 cursor-pointer border border-solid rounded bg-light-blue border-light-brown text-light-white  dark:bg-dark-white dark:border-dark-gray dark:text-dark-black p-2 lg:bg-light-white lg:border-light-blue lg:text-light-blue lg:dark:bg-dark-black lg:dark:border-dark-white lg:dark:text-dark-white
         lg:hover:bg-light-blue lg:hover:border-light-brown lg:hover:text-light-white lg:dark:hover:bg-dark-white lg:dark:hover:border-dark-gray lg:dark:hover:text-dark-black"
-        href="https://www.linkedin.com/in/julian-pinilla-lizarazo-76a11427b/"
+        href="https://www.linkedin.com/in/julianlizarazo/"
         target="_blank"
       >
         <AiFillLinkedin className="w-12 h-12 lg:w-16 lg:h-16" />
