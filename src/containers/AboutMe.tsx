08236434a3cecf7ffb440577700ae4f3debd795a
JulianLizarazo <julianca20122001@hotmail.com>
2023-10-25 13:55:57 -0500

    Add profile image

--- src/containers/AboutMe.tsx
+++ src/containers/AboutMe.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import colombia from "../assets/colombia.png";
+import profile from "../assets/profile.jpeg";
 import ContactButtons from "@/components/ContactButtons";
 import { useTranslations } from "next-intl";
 
@@ -8,7 +8,7 @@ const AboutMe = () => {
     return(
         <section id="about" className="w-full h-[36rem] flex flex-col items-center justify-evenly pt-10 lg:flex-row">
             <section>
-                <Image src={colombia} alt="xd" className="w-[150px] h-[150px] lg:w-[200px] lg:h-[200px]"/>
+                <Image src={profile} alt="Profile image" className="w-[220px] h-[220px] lg:w-[320px] lg:h-[320px] rounded-full"/>
             </section>
             <section>
                 <p className="mx-4 max-w-[600px] text-justify text-lg lg:text-xl">{t("text")}</p>
@@ -16,6 +16,6 @@ const AboutMe = () => {
             </section>
         </section>
     )
-}
+}   
 
 export default AboutMe;
